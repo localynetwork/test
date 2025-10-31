8d1754b8922ef7d041b843de0c5d82ee8c755cb9
Abdi maulana ahmad <maulanaabdi723@gmail.com>
2025-10-31 15:21:51 +0700

    Create test.jsx

---
+++ test.jsx
@@ -0,0 +1,23 @@
+import { Parallax, PArallaxLayer} from '@react-spring/parallax'
+
+function app() {
+  return(
+  
+  <div>
+  <parallax pages = {4}>
+
+    <parallaxlayer>
+    <h2>test</h2>
+    </parallaxlayer>
+
+    <parallaxlayer>
+    <h2>test2</h2>
+    </parallaxlayer>
+    
+  </parallax>
+  
+  </div>
+    
+  );
+  
+}
